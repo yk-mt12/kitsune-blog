ef4c261f7d9de834bcc4f60db3545678732b5c0f
yk-mt12 <agvksl543@gmail.com>
2023-01-13 07:43:58 +0900

    add twitter timeline components

---
+++ src/components/tweet.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Timeline } from "react-twitter-widgets";
+import styled from 'styled-components'
+
+export const TweetTimeline = () => {
+  return (
+    <STimeline >
+      <Timeline
+        dataSource={{
+          sourceType: 'profile',
+          screenName: 'kitsune_yk'
+        }}
+        options={{
+          height: '600',
+          width: '80%'
+        }}
+      />
+    </STimeline>
+  )
+}
+
+const STimeline = styled.div`
+  margin: '0 auto',
+  text-align: center;
+`
