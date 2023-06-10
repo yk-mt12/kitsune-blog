406aeda65096ec873f8413bf16fcac721867c9a5
yk-mt12 <agvksl543@gmail.com>
2023-06-11 02:46:26 +0900

    chore: tagmanegerを追加

--- src/html.js
+++ src/html.js
@@ -11,7 +11,13 @@ export default function HTML(props) {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
-        {/* following is script for using reach-adsense*/}
+        <script>
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-W2XCWH5');
+        </script>
         <script
           async
           src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
